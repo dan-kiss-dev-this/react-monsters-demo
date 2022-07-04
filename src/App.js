f13bef62f86b0622b713d3540c06869a828acc5d
Dan Kiss <flowfast47@gmail.com>
2022-07-04 16:51:47 -0700

    make review monster program

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Linda'
+        },
+        {
+          name: 'Frank'
+        },
+        {
+          name: 'Jacky'
+        }]
+
+    }
+  }
+
+
+
+  render() {
+    // console.log('log')
+    return (
+      <div>
+        {this.state.monsters.map((value, index) => { return <div key={index}>{value.name}</div> })}
+        {/* {this.state.monsters.map(function (value, index) { return <div>{value.name}</div> })} */}
+      </div>
+    )
+  }
 }
 
 export default App;
