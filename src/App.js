16b049c22618289489b7b0418972f0650d6d3ff3
Dan Kiss <flowfast47@gmail.com>
2022-07-05 17:13:01 -0700

    make search case insensitive

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { findRenderedComponentWithType } from 'react-dom/test-utils';
 class App extends Component {
   constructor() {
     super();
-    console.log(1)
     this.state = {
       filteredMonsters: [],
       monsters: [],
@@ -14,25 +13,21 @@ class App extends Component {
     }
   }
 
-
-
   componentDidMount() {
     console.log(2);
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(monstersObject => this.setState({ monsters: monstersObject }))
+      .then(monstersObject => this.setState({ monsters: monstersObject, filteredMonsters: monstersObject }))
   }
-
-
 
   render() {
     const filteringMonsters = e => {
       if(e.target.value) {
         // set search value
-        const searchedValue = e.target.value
+        const searchedValue = e.target.value.toLowerCase();
         console.log(searchedValue);
         // trigger the filter
-        this.setState({ filteredMonsters: this.state.monsters.filter(monster => monster.name.includes(searchedValue) ? monster : null) })
+        this.setState({ filteredMonsters: this.state.monsters.filter(monster => monster.name.toLowerCase().includes(searchedValue) ) })
       } else {
         this.setState({ filteredMonsters: this.state.monsters })
       }
@@ -41,8 +36,6 @@ class App extends Component {
     return (
       <div className="App">
         <input className="search-box" type="search" placeholder="Search monsters" onChange={event => { filteringMonsters(event) }} />
-        {this.state.monsters.map((monster, index) => { return <div key={monster.id}>{monster.name}</div> })}
-        <div>hi</div>
         {this.state.filteredMonsters.map((monster, index) => { return <div key={monster.id}>{monster.name}</div> })}
       </div>
     )
